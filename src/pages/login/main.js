b46e0f1bb1d0ea773be0342c413b1705c3fd0e53
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-09-19 15:59:47 +0900

    feat: 회원가입 로그인 로직 구현 및 자잘한 기능 구현

---
+++ src/pages/login/main.js
@@ -0,0 +1,131 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const loginBtn = document.querySelector('.login_btn');
+
+  if (loginBtn) {
+    loginBtn.addEventListener('click', () => {
+      const idValue = document.getElementById('login_id').value;
+      const pwdValue = document.getElementById('login_password').value;
+
+      // const checkUserInfo = localStorage.getItem(idValue);
+      const checkUserInfo = JSON.parse(localStorage.getItem(idValue));
+
+      if (idValue === checkUserInfo.id && pwdValue === checkUserInfo.password) {
+        location.href = '/src/pages/home/index.html';
+      } else {
+        alert('아이디 혹은 비밀번호를 확인해주세요');
+      }
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const showPwd = document.querySelector('#login_show');
+
+  if (showPwd) {
+    showPwd.addEventListener('click', () => {
+      const changePwdType = document.querySelector('#login_password');
+
+      if (showPwd.id === 'login_show') {
+        changePwdType.setAttribute('type', 'text');
+        showPwd.className = 'fa-solid fa-eye-slash';
+        showPwd.id = 'login_hide';
+      } else {
+        changePwdType.setAttribute('type', 'password');
+        showPwd.className = 'fa-solid fa-eye';
+        showPwd.id = 'login_show';
+      }
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const showPwd = document.querySelector('#signup_show');
+
+  if (showPwd) {
+    showPwd.addEventListener('click', () => {
+      const changePwdType = document.querySelector('#signup_pwd');
+      const changePwdCheckType = document.querySelector('#signup_pwdCheck');
+
+      if (showPwd.id === 'signup_show') {
+        changePwdType.setAttribute('type', 'text');
+        changePwdCheckType.setAttribute('type', 'text');
+        showPwd.className = 'fa-solid fa-eye-slash';
+        showPwd.id = 'signup_hide';
+      } else {
+        changePwdType.setAttribute('type', 'password');
+        changePwdCheckType.setAttribute('type', 'password');
+        showPwd.className = 'fa-solid fa-eye';
+        showPwd.id = 'signup_show';
+      }
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const signupBtn = document.querySelector('.signup_btn');
+
+  if (signupBtn) {
+    signupBtn.addEventListener('click', function () {
+      const idValue = document.getElementById('signup_id').value;
+      const pwdValue = document.getElementById('signup_pwd').value;
+      const pwdCheckValue = document.getElementById('signup_pwdCheck').value;
+
+      console.log(idValue, pwdValue, pwdCheckValue);
+      const userData = {
+        id: idValue,
+        password: pwdValue,
+      };
+
+      if (pwdValue === pwdCheckValue) {
+        localStorage.setItem(`${idValue}`, JSON.stringify(userData));
+        alert('회원가입 성공!');
+      } else {
+        alert('비밀번호가 다릅니다! 확인해주세요😭');
+      }
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const signupBtn = document.querySelector('.signup_ment');
+  const overlay = document.querySelector('.overlay');
+
+  if (signupBtn && overlay) {
+    signupBtn.onclick = moveBlur;
+  }
+
+  function moveBlur() {
+    let keyframes = [
+      { transform: 'translateX(0px)' },
+      { transform: 'translateX(422.5px)' },
+    ];
+    let options = {
+      duration: 500, // 애니메이션 시간
+      easing: 'ease-in-out',
+      fill: 'forwards', // 애니메이션 후 상태 유지
+    };
+    overlay.animate(keyframes, options);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const signupBtn = document.querySelector('.login_ment');
+  const overlay = document.querySelector('.overlay');
+
+  if (signupBtn && overlay) {
+    signupBtn.onclick = moveBlur;
+  }
+
+  function moveBlur() {
+    let keyframes = [
+      { transform: 'translateX(422.5px)' },
+      { transform: 'translateX(0px)' },
+    ];
+    let options = {
+      duration: 500, // 애니메이션 시간
+      easing: 'ease-in-out',
+      fill: 'forwards', // 애니메이션 후 상태 유지
+    };
+    overlay.animate(keyframes, options);
+  }
+});
